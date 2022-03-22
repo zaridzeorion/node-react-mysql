581c044349d85c46e520173909744d301bd96dd1
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-03-22 22:18:24 +0400

    backend: added json parsing

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+// For JSON parsing 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const PORT = 4000; // backend routing port
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
