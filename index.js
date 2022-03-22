400203104aa730da5c5d3b13bd9934096f1505f4
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-03-22 23:19:01 +0400

    backend: connecting routes to index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const routesHandler = require("./routes/handler.js");
 
 const app = express();
 
-// For JSON parsing 
+// For JSON parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Routing
+app.use("/", routesHandler);
 
 const PORT = 4000; // backend routing port
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
